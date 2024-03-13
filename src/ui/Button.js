24c67e4795e392b8a9e1a853cd4f380f1fc82b71
pieceofcakey <choi_927@naver.com>
2024-03-13 17:18:34 +0900

    Feat: Button ui

---
+++ src/ui/Button.js
@@ -0,0 +1,17 @@
+import * as Phaser from "phaser";
+
+export default class Button extends Phaser.GameObjects.Text {
+  constructor(x, y, label, scene, callback) {
+    super(scene, x, y, label);
+
+    this.setOrigin(0.5)
+      .setPadding(10)
+      .setStyle({ backgroundColor: "#027bce", fontSize: 20 })
+      .setInteractive({ useHandCursor: true })
+      .on("pointerover", () => this.setStyle({ fill: "#000000" }))
+      .on("pointerout", () => this.setStyle({ fill: "#ffffff" }))
+      .on("pointerdown", () => callback());
+
+    scene.add.existing(this);
+  }
+}
