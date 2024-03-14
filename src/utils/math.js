4c2dcaf2dead37c2899c725988309d6b1c010cc6
pieceofcakey <choi_927@naver.com>
2024-03-14 11:36:13 +0900

    Feat: math util functions

---
+++ src/utils/math.js
@@ -0,0 +1,14 @@
+import config from "../config";
+
+export function getRandomPosition(x, y) {
+  const randomRadius = Math.random() * Math.PI * 2;
+  const radius =
+    Math.sqrt(config.width * config.width + config.height * config.height) / 2;
+  const randomX = x + radius * Math.cos(randomRadius);
+  const randomY = y + radius * Math.sin(randomRadius);
+  return [randomX, randomY];
+}
+
+export function clamp(value, lo, hi) {
+  return Math.min(Math.max(value, lo), hi);
+}
