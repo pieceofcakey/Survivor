268e056241721b8468ae922f03a36f70b7fd7ceb
pieceofcakey <choi_927@naver.com>
2024-03-14 12:01:51 +0900

    Feat: add explosion effect

---
+++ src/effects/Explosion.js
@@ -0,0 +1,14 @@
+import * as Phaser from "phaser";
+
+export default class Explosion extends Phaser.GameObjects.Sprite {
+  constructor(scene, x, y) {
+    super(scene, x, y, "explosion");
+
+    this.scale = 2;
+    this.setDepth(50);
+
+    this.play("explode");
+
+    scene.add.existing(this);
+  }
+}
